dca15bc4a039be9f380c47a0a33a4585648990b1
yuksekyusuf <68251921+yuksekyusuf@users.noreply.github.com>
2023-10-31 00:45:26 -0400

    create user model and db

--- models.py
+++ models.py
@@ -1,5 +1,19 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+
+
+class Users(Base):
+
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
+
 
 class Todos(Base):
     __tablename__ = 'todos'
@@ -9,5 +23,6 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
     
 
---
+++ routers/auth.py
@@ -0,0 +1,49 @@
+from typing import Annotated
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from models import Users
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from database import Sessionlocal
+from starlette import status
+
+
+router = APIRouter()
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated = 'auto')
+
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+
+def get_db():
+    db = Sessionlocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Sessionlocal, Depends(get_db)]
+
+
+
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency,
+    create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email = create_user_request.email,
+        username = create_user_request.username,
+        first_name = create_user_request.first_name,
+        last_name = create_user_request.last_name,
+        role = create_user_request.role,
+        hashed_password = bcrypt_context.hash(create_user_request.password),
+        is_active = True
+    )
+    db.add(create_user_model)
+    db.commit()
