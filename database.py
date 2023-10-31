cc2a6d7eb18934362fc394b82191489eaeaecef0
yuksekyusuf <68251921+yuksekyusuf@users.noreply.github.com>
2023-10-30 23:56:16 -0400

    config authorization

--- database.py
+++ database.py
@@ -9,3 +9,5 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread
 Sessionlocal = sessionmaker(autocommit=False , autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+print("Authorization")
